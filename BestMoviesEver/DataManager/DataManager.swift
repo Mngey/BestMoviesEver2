7a8f3af6bb4345d0ecab988c983ddee894e6c06a
mngey <mitrofanova92@gmail.com>
2020-09-04 12:16:50 +0300

    best (not really) movie list

--- BestMoviesEver/DataManager/DataManager.swift
+++ BestMoviesEver/DataManager/DataManager.swift
@@ -66,23 +66,3 @@ class DataManager {
 }
 
 
-//Alien (1979) Ridley Scott
-//Avengers: Endgame (2019) Anthony Russo, Joe Russo
-//Ginger Snaps (2000)  John Fawcett
-//Gone Girl (2014) David Fincher
-//Inglourious Basterds (2009) Quentin Tarantino
-//Jojo Rabbit (2019)  Taika Waititi
-//Kiss Kiss Bang Bang (2005) Shane Black
-//Once Upon a Time... In Hollywood (2019)
-//Pulp Fiction (1994) Fight Club (1999)
-//Seven (1995)
-//Silver Linings Playbook (2012) David O. Russell
-//SLC Punk! (1998) James Merendino
-//Space Jam (1996) Joe Pytka
-//The Fall (2006) Tarsem Singh
-//The Matrix (1999) The Wachowski Brothers
-
-
-
-
-
